50700c14d9b449c145c33bc7c94a04bf34b21eab
十七 <zjack6173@gmail.com>
2026-08-30 11:55:11 +0800

    phase1: export runtime jobs and leases

--- commercial_v1/runtime/__init__.py
+++ commercial_v1/runtime/__init__.py
@@ -1,1 +1,16 @@
 """商业版运行时基础设施。"""
+
+from .jobs import ClaimedJob, PersistentJobStore, StaleJobFencingToken
+from .leases import Lease, LeaseConflict, LeaseManager, StaleFencingToken
+from .single_instance import GlobalUserMutex
+
+__all__ = [
+    "ClaimedJob",
+    "PersistentJobStore",
+    "StaleJobFencingToken",
+    "Lease",
+    "LeaseConflict",
+    "LeaseManager",
+    "StaleFencingToken",
+    "GlobalUserMutex",
+]
